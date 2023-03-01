ad77b8c5ca1ec7649b13144d8229924fd8257b8a
Aung S Khaing <cs15lwi23aki@ieng6-201.ucsd.edu>
2023-03-01 11:38:25 -0800

    added human-friendly progress report messages in grade script

--- grade.sh
+++ grade.sh
@@ -3,7 +3,8 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
-
+echo '-----------------------------------'
+echo 'Test File Exist:'
 cd student-submission
 if [[ -f ListExamples.java ]]
 then 
@@ -12,10 +13,18 @@ else
 echo "ListExamples.java not found"
 exit 1
 fi
+
+echo '-----------------------------------'
+echo 'Copying files into tester directory...'
 cp ../TestListExamples.java ./
 cp ../GradeServer.java ./
 cp ../Server.java ./
 cp -R ../lib ./
+
+echo 'File copy complete'
+echo '-----------------------------------'
+echo 'Compile Test'
+
 javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
 if [[ $? != 0 ]]
 then 
@@ -24,5 +33,8 @@ then
 else 
 echo "the files compile"
 fi
+
+echo '-----------------------------------'
+echo 'JUnit Test Results:'
 
 java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
